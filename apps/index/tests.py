df8b196fdaef36654ee94d0535732e0330349be4
Oleksandr PIkovets <opikovets@spscommerce.com>
2017-03-11 10:01:13 +0200

    Added geneirc test for index view

--- apps/index/tests.py
+++ apps/index/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexViewTests(TestCase):
+
+    def test_index_general(self):
+        """
+        Index page should exist
+        """
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
